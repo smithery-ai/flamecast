cba59377d9c1a902ab4205bb0abb64a0661d3df1
Anirudh Kamath <github@kamath.io>
2026-04-09 12:59:49 -0700

    Add email worker support (#171)
    * Add email worker support
    
    * fmt

--- apps/bridge/src/worker.ts
+++ apps/bridge/src/worker.ts
@@ -1,5 +1,11 @@
 import { app } from "./app.js";
+import { handleEmail } from "./lib/email-handler.js";
+import type { Env } from "./types.js";
 
 export default {
   fetch: app.fetch,
+
+  async email(message: ForwardableEmailMessage, env: Env): Promise<void> {
+    await handleEmail(message, env);
+  },
 };
